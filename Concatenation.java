d45553482a548974e074dae8a028b57675039e69
kk6602139-lang <kk6602139@gmail.com>
2026-07-22 05:49:56 +0530

    Create Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        String Str="hello";
+        String Str1="Hello";
+        String Str2=Str+Str1;
+        System.out.println(Str+""+Str1);
+    }
+}
